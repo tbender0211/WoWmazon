12fa5b19fa9b12ba84ba86b59034814e4b48eff7
Cassindi <tara.devin.b@gmail.com>
2018-08-03 14:47:49 -0500

    yay callbacks

--- WoWmazonCustomer.js
+++ WoWmazonCustomer.js
@@ -2,22 +2,25 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
+
     host: "localhost",
     port: 3306,
     user: "root",
     password: "NodeNerd1993",
     database: "WoWmazon_db"
+
 });
+
 
 
 connection.connect(function(err){
 
     if(err) throw err;
-    itemPurchase();
 
 });
 
-var productDisplay = function(){
+//Function that displays products from the WoWmazon Database
+function productDisplay(){
 
     var query = "SELECT * FROM products";
 
@@ -32,16 +35,21 @@ var productDisplay = function(){
             console.log(res[i].item_id + ". " + res[i].product_name);
             
         }
+
+        console.log("------------------");
+        itemPurchase();
+
     });
 };
 
+//Function where user chooses an item to purchase
 function itemPurchase(){
 
     inquirer.prompt([
 
     {
         type:"confirm",
-        message: "Welcome to the Horde Auction House! Would you like to view available items?",
+        message: "Welcome to the Horde Auction House! Would you like to purchase an item?",
         name:"confirm",
         default: true
     }
@@ -51,27 +59,51 @@ function itemPurchase(){
 
         if(inquirerResponse.confirm){
             
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "id",
+                    message: "What is the number of the item you would like to purchase?"
+                },
             
-            productDisplay();
-            
+                {
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you like?"
+                }
+            ]).then(function(inquirerResponse){
+
+                    var selectedProduct = parseInt(inquirerResponse.id);
+                    var selectedQuantity = parseInt(inquirerResponse.quantity);
+
+                    console.log(selectedProduct);
+                    console.log(selectedQuantity);
+
+                    function updateInventory(){
+    
+                        var query = "SELECT * FROM products WHERE item_id = ?";
+                        connection.query(query, [selectedProduct], function(err,res){
+                            if (err) throw err;
+                            console.log(res);
+                    })
+
+                    }
+
+                    updateInventory();
+
+            });
             
         }
     })
 
 };
 
-function purchasePrompt(){
-    inquirer.prompt([
-    {
-        type: "input",
-        name: "id",
-        message: "What is the number of the item you would like to purchase?"
-    },
+//Function that matches id chosen from the database and updates quantity
 
-    {
-        type: "input",
-        name: "quantity",
-        message: "How many would you like?"
-    }
-    ]);
-}
+
+
+function runWowmazon(){
+    productDisplay();
+};
+
+runWowmazon();
