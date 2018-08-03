c6fa96f0c657f0535e247ab39a7c2e30ab489ff2
Cassindi <tara.devin.b@gmail.com>
2018-08-03 14:13:59 -0500

    ugh

--- WoWmazonCustomer.js
+++ WoWmazonCustomer.js
@@ -6,24 +6,72 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "NodeNerd1993",
-    database: "bamazon_db"
+    database: "WoWmazon_db"
 });
+
 
 connection.connect(function(err){
+
     if(err) throw err;
-    productDisplay();
+    itemPurchase();
+
 });
 
-function productDisplay(){
+var productDisplay = function(){
 
     var query = "SELECT * FROM products";
 
     connection.query(query, function(err, res){
 
+        console.log("\nHorde Auction House\n");
+        console.log("Available Items: ")
+        console.log("----------------\n")
+
         for (i=0; i < res.length; i++){
 
-            console.log(res[i].product_name);
+            console.log(res[i].item_id + ". " + res[i].product_name);
             
         }
     });
 };
+
+function itemPurchase(){
+
+    inquirer.prompt([
+
+    {
+        type:"confirm",
+        message: "Welcome to the Horde Auction House! Would you like to view available items?",
+        name:"confirm",
+        default: true
+    }
+    
+
+    ]).then(function(inquirerResponse){
+
+        if(inquirerResponse.confirm){
+            
+            
+            productDisplay();
+            
+            
+        }
+    })
+
+};
+
+function purchasePrompt(){
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "id",
+        message: "What is the number of the item you would like to purchase?"
+    },
+
+    {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like?"
+    }
+    ]);
+}
--- WoWmazon_db.sql
+++ WoWmazon_db.sql
@@ -11,5 +11,21 @@ CREATE TABLE products (
     price decimal(10, 2),
     stock_quantity int(10),
     primary key (item_id)
-)
+);
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Primal Mana", "Trade Goods", 323.63, 97),
+		("Siamese Cat (Common)", "Battle Pets", 88.80, 8),
+        ("Nightmare Whelpling", "Battle Pets", 49998.01, 8),
+        ("Copper Ore (50)", "Trade Goods", 2500, 7),
+        ("Flask of the Countless Armies", "Consumables", 147.99, 16),
+        ("Lavish Suramar Feast", "Consumables", 99.19, 258),
+        ("Rough Boomstick", "Weapons", 2, 10),
+        ("Ranseur of Hatred", "Weapons", 149.74, 6),
+        ("Replica Deathmist Mask", "Armor", 59998.49, 2),
+        ("Saber's Eye of Agility", "Gems", 2695.99, 15),
+        ("Deadly Eye of Prophecy", "Gems", 379.50, 147),
+        ("Runescale Koi (200)", "Trade Goods", 26.15, 5),
+        ("Schematic: Lightning Etched Specs", "Recipes", 8401.33, 2);
+        
+SELECT * FROM products;
